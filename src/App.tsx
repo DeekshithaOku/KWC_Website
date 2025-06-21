604e35c542b15fc18bfc60956c1df47141c05a91
Rishik15 <ry248@njit.edu>
2025-06-20 21:04:10 -0400

    Static Background done

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes location={location} key={location.pathname}>
+          <Route path="/" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/Home.tsx
@@ -0,0 +1,10 @@
+import { Background } from "../components/Background";
+
+export const Home = () => {
+  return (
+    <div className="relative w-full h-screen overflow-hidden bg-gradient-to-b from-[#74B9E5] to-[#80CFEB]">
+      <Background />
+      {/* Your content here if needed */}
+    </div>
+  );
+};
