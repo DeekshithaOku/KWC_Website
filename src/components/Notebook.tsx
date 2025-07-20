ee4306f627d21b0da115510558c6baf3e6d063d4
Rishik15 <ry248@njit.edu>
2025-07-20 17:54:30 -0400

    Changed the logic of binders

--- src/components/Notebook.tsx
+++ src/components/Notebook.tsx
@@ -1,33 +1,28 @@
-
 export const NotebookPaper = ({ children }: { children: React.ReactNode }) => {
   return (
-    <>
+    <div
+      className="relative z-10 top-[20%] left-1/2 transform -translate-x-1/2 
+      w-[325px] sm:w-[450px] md:w-[600px] lg:w-[800px] xl:w-[1000px]
+      bg-[#fffef9] rounded-t-3xl shadow-xl border border-[#e0dccc] overflow-visible"
+    >
       <img
         src="/src/assets/binder.png"
         alt="Binder Ring"
-        className="absolute top-[18.5%] md:top-[18%] lg:top-[16%] left-[25%] md:w-[50px] w-[25px] h-auto z-20"
+        className="absolute top-[-14px] md:top-[-28px] left-[25%] md:w-[50px] w-[25px] h-auto z-20"
       />
       <img
         src="/src/assets/binder.png"
         alt="Binder Ring"
-        className="absolute top-[18.5%] md:top-[18%] lg:top-[16%] left-[50%] md:w-[50px] w-[25px] h-auto z-20"
+        className="absolute top-[-14px] md:top-[-28px] left-[50%] md:w-[50px] w-[25px] h-auto z-20"
       />
       <img
         src="/src/assets/binder.png"
         alt="Binder Ring"
-        className="absolute top-[18.5%] md:top-[18%] lg:top-[16%] left-[75%] md:w-[50px] w-[25px] h-auto z-20"
+        className="absolute top-[-14px] md:top-[-28px] left-[75%] md:w-[50px] w-[25px] h-auto z-20"
       />
-
-      <div
-        className="relative z-10 top-[20%]  left-1/2 transform -translate-x-1/2 
-        w-[325px] sm:w-[450px] md:w-[600px] lg:w-[800px] xl:w-[1000px]
-        bg-[#fffef9] rounded-t-3xl shadow-xl border border-[#e0dccc] overflow-visible"
-      >
-        <div className="relative paper-lines mt-[84px] px-4 pb-8 pt-4">
-          {children}
-        </div>
+      <div className="relative paper-lines mt-[64px] px-4 pb-8 pt-4">
+        {children}
       </div>
-    </>
+    </div>
   );
 };
-
--- src/components/TripsNotebook.tsx
+++ src/components/TripsNotebook.tsx
@@ -8,40 +8,38 @@ export const TripsNotebook = ({
   color: string;
 }) => {
   return (
-    <>
+    <motion.div
+      key={color}
+      initial={{ opacity: 1 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 1 }}
+      className="relative z-10 top-[20%] left-1/2 transform -translate-x-1/2 
+        w-[260px] md:w-[600px] lg:w-[800px] xl:w-[1000px]
+        rounded-tl-3xl rounded-bl-3xl shadow-xl border-[1.5px] overflow-visible min-h-[500px] items-center"
+      style={{
+        backgroundColor: `${color}A6`,
+        borderColor: color,
+      }}
+    >
       <img
         src="/src/assets/binder.png"
         alt="Binder Ring"
-        className="absolute top-[18.5%] md:top-[18%] lg:top-[16%] left-[27%] md:w-[50px] w-[25px] h-auto z-20"
+        className="absolute top-[-14px] md:top-[-28px] left-[25%] md:w-[50px] w-[25px] h-auto z-20"
       />
       <img
         src="/src/assets/binder.png"
         alt="Binder Ring"
-        className="absolute top-[18.5%] md:top-[18%] lg:top-[16%] left-[47%] md:w-[50px] w-[25px] h-auto z-20"
+        className="absolute top-[-14px] md:top-[-28px] left-[50%] md:w-[50px] w-[25px] h-auto z-20"
       />
       <img
         src="/src/assets/binder.png"
         alt="Binder Ring"
-        className="absolute top-[18.5%] md:top-[18%] lg:top-[16%] left-[67%] md:w-[50px] w-[25px] h-auto z-20"
+        className="absolute top-[-14px] md:top-[-28px] left-[75%] md:w-[50px] w-[25px] h-auto z-20"
       />
 
-        <motion.div
-          key={color}
-          initial={{ opacity: 1 }}
-          animate={{ opacity: 1}}
-          exit={{ opacity: 1 }}
-          className="relative z-10 top-[20%] left-1/2 transform -translate-x-1/2 
-      w-[260px] md:w-[600px] lg:w-[800px] xl:w-[1000px]
-      rounded-tl-3xl rounded-bl-3xl shadow-xl border-[1.5px] overflow-visible min-h-[500px] items-center"
-          style={{
-            backgroundColor: `${color}A6`,
-            borderColor: color,
-          }}
-        >
-          <div className="relative mt-[64px] md:px-4 pb-8 pt-4">
-            <AnimatePresence mode="wait">{children}</AnimatePresence>
-          </div>
-        </motion.div>
-    </>
+      <div className="relative mt-[64px] md:px-4 pb-8 pt-4">
+        <AnimatePresence mode="wait">{children}</AnimatePresence>
+      </div>
+    </motion.div>
   );
 };
